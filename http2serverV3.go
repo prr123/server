b5b662f19a2a013649cd75fe1338c062386ecc5e
prr123 <priemen@gmail.com>
2022-08-09 14:04:43 +0200

    V3 added response fpr post, display of favicon

---
+++ http2serverV3.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type httpObj struct {
+	dbg bool
+	baseUri string
+}
+
+func initHttp()(sObj *httpObj) {
+
+	sObj = new(httpObj)
+	sObj.dbg = false
+
+	return sObj
+}
+
+func (sObj *httpObj) handle(w http.ResponseWriter, r *http.Request) {
+
+	// Log the request protocol
+	if sObj.dbg {
+		fmt.Printf(" *** Received connection request from: %v from: %s\n",r.RemoteAddr, r.Proto)
+		fmt.Println("  Request URI:    ", r.RequestURI)
+		fmt.Println("  Request URL:    ", r.URL, " Host: ", r.Host)
+		fmt.Println("  Request Method: ", r.Method)
+
+		fmt.Println("  *** headers *** ")
+		for k, v := range r.Header {
+			fmt.Printf("    %-25s : %-25s \n", k, v)
+		}
+	}
+
+	// Send a message back to the client
+	//	w.Write([]byte("Hello"))
+	// now I can pass parameters via sObj
+
+	if r.Method == "GET" {
+		p := r.RequestURI
+		if p == "/" {
+			p = "/home/peter/www/azul/html/index.html"
+		}
+		fmt.Printf("\n*** serving %s ***\n",p)
+    	http.ServeFile(w, r, p)
+		return
+	}
+
+	if r.Method == "POST" {
+		fmt.Printf("\n*** received post ***\n")
+		return
+	}
+
+}
+
+
+func main() {
+
+	var serveUri string
+	argsNum := len(os.Args)
+
+	// initialize server  object
+	sObj := initHttp()
+	if sObj == nil {
+		fmt.Printf("error init sObj!\n")
+		os.Exit(-1)
+	}
+	baseUri :="/home/peter/www/"
+
+	switch argsNum {
+		case 1:
+			serveUri = baseUri + "base"
+		case 2:
+			if os.Args[1] == "dbg" {
+				sObj.dbg = true
+			} else {
+				serveUri = baseUri + os.Args[1]
+			}
+		case 3:
+			serveUri = baseUri + os.Args[1]
+			if os.Args[2] == "dbg" {sObj.dbg = true}
+
+		default:
+			fmt.Println("error cmd line too many args!")
+			os.Exit(-1)
+	}
+
+	_,err := os.Stat(serveUri)
+	if err != nil {
+		fmt.Printf("error %v\n", err)
+		os.Exit(-1)
+	}
+
+	fmt.Printf("Uri is %s\n", serveUri)
+	// Create a server on port 8000
+	// Exactly how you would run an HTTP/1.1 server
+	srv := &http.Server{Addr: "127.0.0.1:8002", Handler: http.HandlerFunc(sObj.handle)}
+
+	// Start the server with TLS, since we are running HTTP/2 it must be
+	// run with TLS.
+	// Exactly how you would run an HTTP/1.1 server with TLS connection.
+	fmt.Printf("Serving localhost on https: 127.0.0.1:8002 \n")
+	err = srv.ListenAndServeTLS("/home/peter/newca/server_crt.pem", "/home/peter/newca/server_key.pem")
+	if err != nil {
+		fmt.Printf("error starting server: %v\n", err)
+		os.Exit(-11)
+	}
+}
+
